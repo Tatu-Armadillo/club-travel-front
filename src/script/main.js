0878af633a9a04901ec994b479cc10c7a23dff22
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-01-20 22:36:50 -0400

    feat: funcionalidade do pop up a cada load

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,15 @@
-document.querySelector(".mobile-menu").addEventListener("click", handleClickActive)
+document
+  .querySelector(".mobile-menu")
+  .addEventListener("click", handleClickActive);
 
 function handleClickActive() {
-    const menuMobile = document.querySelector(".nav-list");
-    const burguer = document.querySelector(".mobile-menu");
+  const menuMobile = document.querySelector(".nav-list");
+  const burguer = document.querySelector(".mobile-menu");
 
-    menuMobile.classList.toggle("active");
-    burguer.classList.toggle("active");
+  menuMobile.classList.toggle("active");
+  burguer.classList.toggle("active");
 }
+
+document.querySelector("body").onload = () => {
+  document.querySelector("dialog").showModal();
+};
