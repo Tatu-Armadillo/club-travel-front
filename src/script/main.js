fe5eb136de1a3f26e839c8e443c966ba7ef51267
João Ygor <ygorvieira2002@gmail.com>
2023-01-27 14:40:53 -0400

    fix: handleClick > header

--- src/script/main.js
+++ src/script/main.js
@@ -1,18 +1,5 @@
-import { gereneteComponent } from "./components.js";
+import { gereneteHomePage } from "./home-page.js";
 
-window.addEventListener("load", () => gereneteComponent());
-
-// document.querySelector('.mobile-menu').addEventListener('click', handleClickActive);
+window.addEventListener("load", () => gereneteHomePage());
 
 document.querySelector('body').onload = () => document.querySelector('dialog').showModal();
-
-document.querySelector('.btn-close-modal').onclick = () => document.querySelector('dialog').close();
-
-function handleClickActive() {
-    const menuMobile = document.querySelector('.nav-list');
-    const burguer = document.querySelector('.mobile-menu');
-
-    menuMobile.classList.toggle('active');
-    burguer.classList.toggle('active');
-}
-
