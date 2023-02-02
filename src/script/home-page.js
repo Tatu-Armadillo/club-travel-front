0c842a6d57f7fe380e196afdc3e23be7a9c4c003
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-02-02 00:00:27 -0400

    fix: call head function

--- src/script/home-page.js
+++ src/script/home-page.js
@@ -1,10 +1,25 @@
-import { header } from "./shared/header.js";
-import { footer } from "./shared/footer.js";
-import { head } from "./shared/head.js";
+import { header } from './shared/header.js';
+import { footer } from './shared/footer.js';
+import { head, links } from './shared/head.js';
+
+const linksConfig = [
+    links('shortcut icon', 'images/logo/logo-travel.png', 'image/x-icon'),
+    links('stylesheet', './styles/reset.css', ''),
+    links('stylesheet', './styles/global.css', ''),
+    links('stylesheet', './styles/header.css', ''),
+    links('stylesheet', './styles/footer.css', ''),
+    links('stylesheet', './styles/main-content.css', ''),
+    links('stylesheet', './styles/text-presets.css', ''),
+    links(
+        'stylesheet',
+        'https://fonts.googleapis.com/css2?family=Montserrat&display=swap',
+        ''
+    ),
+];
 
 export function gereneteHomePage() {
-    document.head.append(head());
+    head(linksConfig);
     document.body.prepend(header());
     document.body.append(footer());
     // document.querySelector('.btn-close-modal').onclick = () => document.querySelector('dialog').close();
-};
+}
