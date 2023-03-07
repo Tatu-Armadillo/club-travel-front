ea0c8a64fbc28b417b46dab353ef69e9590780e1
João Ygor <ygorvieira2002@gmail.com>
2023-03-07 19:38:34 -0400

    fix: strutured

--- club-travel-front/src/App.tsx
+++ club-travel-front/src/App.tsx
@@ -1,8 +1,15 @@
+import { Header } from "@/shared/Components";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mainContent">
+      <Header />
+
+      <main>
+
+      </main>
+      
+    </div>
   )
 }
 
