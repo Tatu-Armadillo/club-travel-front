f90c489feb39fc32cdc9350856657137313b4a9d
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-07 02:04:07 -0400

    feat: GridContainer

---
+++ club-travel-front/src/shared/Components/Main/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+export const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 60% 30%;
+    justify-content: space-around;
+    padding: 1rem;
+    height: 100%;
+`;
