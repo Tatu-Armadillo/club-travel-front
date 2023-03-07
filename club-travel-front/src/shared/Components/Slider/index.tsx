04fffc36bc24774f1547b0dd1347e1c1656587cc
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-07 02:03:38 -0400

    feat: Slider

---
+++ club-travel-front/src/shared/Components/Slider/index.tsx
@@ -0,0 +1,15 @@
+import { Swiper } from 'swiper/react';
+import { SliderProps } from '../../Interface/ISlider';
+import { Navigation, Pagination, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './styles.css';
+
+export const Slider = ({ settings, children }: SliderProps) => {
+    return (
+        <Swiper modules={[Navigation, Pagination, A11y]} {...settings}>
+            {children}
+        </Swiper>
+    );
+};
