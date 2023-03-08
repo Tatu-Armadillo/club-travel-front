c7a19bac65fb5b3f3a362e1e0b5b095891ccfc29
João Ygor <ygorvieira2002@gmail.com>
2023-03-08 16:50:41 -0400

    fix: finish styles navbar

--- club-travel-front/src/shared/Components/Header/header.styled.ts
+++ club-travel-front/src/shared/Components/Header/header.styled.ts
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
+type Props = {
+    open: boolean
+};
+
 export const Navbar = styled.nav`
-    
+    background-color: #03258C;
+`;
+
+export const BoxImages = styled.div`
+    a {
+        display: flex;
+        align-items: center;
+        gap: .4rem;
+    }
+`;
+
+export const BoxLogo = styled.img`
+    height: 3.5rem;
+    width: auto;
+`;
+
+export const BoxMobile = styled.div<Props>`
+    display: ${(props) => props.open ? "block" : "none"};
 `;
