b548588a97d9d8420e7d86236486483aac3bbd8b
João Ygor <ygorvieira2002@gmail.com>
2023-03-08 16:50:15 -0400

    fix: browserRouter

--- club-travel-front/src/main.tsx
+++ club-travel-front/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import GlobalSyled from "./styles/global";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalSyled />
-    <App />
+    <BrowserRouter>
+      <GlobalSyled />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
