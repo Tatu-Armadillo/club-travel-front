9c530709f1fc1bc16ce485d74cc835149da22c8c
João Ygor <ygorvieira2002@gmail.com>
2023-03-10 10:48:48 -0400

    feat: exports pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
-export { Home } from './Home/home';
+export { Home } from "./Home/home";
+export { About } from "./About/about"
+export { Infographics } from "./Infographics/infographics";
+export { SignIn } from "./SignIn/signIn";
