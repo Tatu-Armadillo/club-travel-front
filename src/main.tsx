92d9e6f517ddf66d6d0dc1c62147beb809f74fee
João Ygor <ygorvieira2002@gmail.com>
2023-03-10 10:13:08 -0400

    fix: strutured new

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Footer, Header } from '@/shared/Components';
 import './styles/index.css';
 
 import { BrowserRouter } from 'react-router-dom';
-import { Router } from './src/routes/Router';
+import { Router } from './routes/Router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
