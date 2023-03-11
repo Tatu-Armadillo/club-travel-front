5bdeb7d12e0198089d8410b26a05f9c0d6a78a0e
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-11 00:18:35 -0400

    feat: singIn base component

--- src/pages/SignIn/signIn.styled.ts
+++ src/pages/SignIn/signIn.styled.ts
@@ -1,1 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const MainContainr = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 40vw;
+    height: 70vh;
+    border-radius: 1rem;
+    margin: 1rem;
+    border: 4px solid #03258c;
+    background-color: rgb(255, 255, 255);
+
+    &&:hover {
+        box-shadow: 30px 30px 20px rgba(0, 0, 0, 0.3);
+        transform: translateY(-1%);
+    }
+`;
--- src/pages/SignIn/signIn.tsx
+++ src/pages/SignIn/signIn.tsx
@@ -1,5 +1,16 @@
+import { MainContainr } from './signIn.styled';
+import { FaUser } from 'react-icons/fa';
+
 export const SignIn = () => {
     return (
-        <h1>SignIn Works!</h1>
+        <div className='flex items-center justify-center'>
+            <MainContainr>
+                <span className='flex gap-2'>
+                    <FaUser size={'2em'} />
+                    <p className='text-xl font-bold pt-1'>LOGIN</p>
+                </span>
+                <hr className='h-2 w-14 bg-black pt' />
+            </MainContainr>
+        </div>
     );
 };
