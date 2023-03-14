aba999d82321fc6239d33d673c752aaa819b2d88
João Ygor <ygorvieira2002@gmail.com>
2023-03-14 18:00:24 -0400

    fix: about incomplete

--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,5 +1,29 @@
+import { BoxContainer, BoxTitle, Card } from "./about.styled";
+
 export const About = () => {
     return (
-        <h1>About Works!</h1>
+        <BoxContainer>
+            <BoxTitle>
+                <h1>Sobre a Travel Club</h1>
+                <p>Conheça mais sobre o grupo Travel</p>
+            </BoxTitle>
+            <div className="grid lg:grid-cols-3 justify-items-center grid-cols-1 lg:gap-0 gap-4 py-10">
+                <Card>
+                    <h3>title</h3>
+                    <hr className="my-2 border-slate-500" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!</p>
+                </Card>
+                <Card>
+                    <h3>title</h3>
+                    <hr className="my-2 border-slate-500" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!</p>
+                </Card>
+                <Card>
+                    <h3>title</h3>
+                    <hr className="my-2 border-slate-500" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!</p>
+                </Card>
+            </div>
+        </BoxContainer>
     );
 };
