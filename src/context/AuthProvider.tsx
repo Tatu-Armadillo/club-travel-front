25fddcef1f728ee6fa998f9c00c6d217570e0409
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-13 23:50:22 -0400

    feat: saving localStorage authToken

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,14 +1,30 @@
 import { User } from '@/shared/Interface/IAuthContext';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { api } from '@/shared/Service/index';
+import { useApi } from '@/shared/hooks/useApi';
+import { setToken } from '@/shared/services/setToken';
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
+    const api = useApi();
     const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        const hasToken = async () => {
+            const saveData = localStorage.getItem('authenticationToken');
+            if (saveData) {
+                const data = await api.validateToken(saveData);
+                if (data.user) {
+                    setUser(data.user);
+                }
+            }
+        };
+
+        hasToken();
+    }, []);
 
     const signIn = async (name: string, password: string) => {
         const data = await api.signIn(name, password);
-        if (data.user && data.request_token) {
+        if (data.user && data.token) {
             setUser(data.user);
+            setToken(data.token);
             return true;
         }
         return false;
@@ -16,6 +32,7 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 
     const signOut = async () => {
         await api.signOut();
+        setToken('');
         setUser(null);
     };
     return (
