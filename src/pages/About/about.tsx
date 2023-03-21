e3d4057d518c430e1d6fdf1032c0475ab8ea185b
João Ygor <ygorvieira2002@gmail.com>
2023-03-20 20:35:32 -0400

    feat: render Card

--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,6 +1,18 @@
-import { BoxContainer, BoxTitle, Card, BoxCards, BoxInfo, InfoText, InfoImg } from "./about.styled";
+import { BoxContainer, BoxTitle, BoxCards, BoxInfo, InfoText, InfoImg } from "./about.styled";
+import { CardAbout } from "@/shared/Components";
 
 export const About = () => {
+    type Props = {
+        title: string,
+        paragraph: string
+    };
+
+    const cardsArr: Props[] = [
+        { title: "title", paragraph: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!" },
+        { title: "title", paragraph: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!" },
+        { title: "title", paragraph: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!" }
+    ];
+
     return (
         <BoxContainer>
             <BoxTitle>
@@ -8,32 +20,23 @@ export const About = () => {
                 <p>Conheça mais sobre o grupo Travel</p>
             </BoxTitle>
             <BoxCards className="grid lg:grid-cols-3 grid-cols-1 lg:gap-0 gap-4">
-                <Card>
-                    <h3>title</h3>
-                    <hr className="my-2 border-slate-500" />
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!</p>
-                </Card>
-                <Card>
-                    <h3>title</h3>
-                    <hr className="my-2 border-slate-500" />
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!</p>
-                </Card>
-                <Card>
-                    <h3>title</h3>
-                    <hr className="my-2 border-slate-500" />
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus natus amet dolor earum pariatur aliquid laboriosam nam repellat repudiandae consequatur voluptatum alias tempore officia, ut facere sequi nihil quo delectus!</p>
-                </Card>
+                {cardsArr.map((card, key) => {
+                    const { paragraph, title } = card;
+                    return (
+                        <CardAbout key={key} paragraph={paragraph} title={title} />
+                    )
+                })}
             </BoxCards>
 
             <div className="grid grid-rows-4 gap-10 justify-items-center lg:mx-0 mx-10 ">
-                <BoxInfo className="lg:flex-row flex flex-col-reverse ">
-                    <InfoText className="bg-black text-white lg:rounded-tl-xl lg:rounded-br-none">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, unde. Distinctio tempora labore quae laborum nam mollitia beatae iure? Voluptatibus doloremque itaque, distinctio rem saepe sapiente blanditiis cumque qui dolore.</InfoText>
-                    <InfoImg src="#" className="bg-amber-500 rounded-tl-xl rounded-br-xl" />
-                </BoxInfo>
-{/* 
                 <BoxInfo>
-                    <InfoImg src="#" className="bg-black text-white" />
-                    <InfoText className="bg-amber-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, unde. Distinctio tempora labore quae laborum nam mollitia beatae iure? Voluptatibus doloremque itaque, distinctio rem saepe sapiente blanditiis cumque qui dolore.</InfoText>
+                    <InfoText>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, unde. Distinctio tempora labore quae laborum nam mollitia beatae iure? Voluptatibus doloremque itaque, distinctio rem saepe sapiente blanditiis cumque qui dolore.</InfoText>
+                    <InfoImg src="#" className="bg-amber-500" />
+                </BoxInfo>
+
+                <BoxInfo className="inverter">
+                    <InfoImg src="#" className="leftImg bg-amber-500" />
+                    <InfoText className="leftTxt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, unde. Distinctio tempora labore quae laborum nam mollitia beatae iure? Voluptatibus doloremque itaque, distinctio rem saepe sapiente blanditiis cumque qui dolore.</InfoText>
                 </BoxInfo>
 
                 <BoxInfo>
@@ -41,10 +44,10 @@ export const About = () => {
                     <InfoImg src="#" className="bg-amber-500" />
                 </BoxInfo>
 
-                <BoxInfo>
-                    <InfoImg src="#" className="bg-black text-white" />
-                    <InfoText className="bg-amber-500 ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, unde. Distinctio tempora labore quae laborum nam mollitia beatae iure? Voluptatibus doloremque itaque, distinctio rem saepe sapiente blanditiis cumque qui dolore.</InfoText>
-                </BoxInfo> */}
+                <BoxInfo className="inverter">
+                    <InfoImg src="#" className="leftImg bg-amber-500" />
+                    <InfoText className="leftTxt">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, unde. Distinctio tempora labore quae laborum nam mollitia beatae iure? Voluptatibus doloremque itaque, distinctio rem saepe sapiente blanditiis cumque qui dolore.</InfoText>
+                </BoxInfo>
             </div>
         </BoxContainer>
     );
