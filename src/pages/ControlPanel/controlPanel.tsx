26a9547d32bbc2681d3404a6715ab22e877ffc0b
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-21 12:13:34 -0400

    feat: FormRouter

--- src/pages/ControlPanel/controlPanel.tsx
+++ src/pages/ControlPanel/controlPanel.tsx
@@ -1,21 +1,14 @@
 import { AuthContext } from '@/context/AuthContext';
-import React, { useState, MouseEvent } from 'react';
 import { useContext } from 'react';
-import { Button, Flex, Box } from '@chakra-ui/react';
-import { BiArrowFromLeft } from 'react-icons/bi';
-import BackGround from '@/shared/img/image-background-panel.jpg';
+import React, { useState } from 'react';
+import { FormRouter } from '@/routes/FormRouter';
 
 export const ControlPanel = () => {
     const [timerLogin, setTimerLogin] = useState(true);
     setTimeout(() => {
         setTimerLogin(false);
     }, 4000);
-    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-        if (e.currentTarget.name === 'newsButton')
-            console.log('clicou no news');
-        if (e.currentTarget.name === 'destinationButton')
-            console.log('clicou no destinations');
-    };
+
     const auth = useContext(AuthContext);
     return (
         <React.Fragment>
@@ -28,56 +21,7 @@ export const ControlPanel = () => {
                     , Bem vindo ao seu painel de controle
                 </h1>
             ) : null}
-            <Flex
-                align='center'
-                justify='center'
-                direction='column'
-                minHeight='80vh'
-                width='full'
-                // bgImage={`url( ${BackGround})`}
-                bgColor={'white'}
-            >
-                <Box
-                    display='flex'
-                    flexDir='column'
-                    width='full'
-                    bgColor='gray'
-                    borderRadius='1rem'
-                >
-                    <Button
-                        width='20rem'
-                        height='10rem'
-                        padding='1rem'
-                        size='md'
-                        name='newsButton'
-                        onClick={handleClick}
-                    >
-                        Formulário de Notícias
-                        <BiArrowFromLeft color='white' />
-                    </Button>
-                    <Button
-                        width='20rem'
-                        height='10rem'
-                        padding='1rem'
-                        size='md'
-                        name='destinationButton'
-                        onClick={handleClick}
-                    >
-                        Formulário de Destinos
-                        <BiArrowFromLeft color='white' />
-                    </Button>
-                    <Button
-                        width='20rem'
-                        height='10rem'
-                        padding='1rem'
-                        size='md'
-                        onClick={handleClick}
-                    >
-                        Formulário de Notícias
-                        <BiArrowFromLeft color='white' />
-                    </Button>
-                </Box>
-            </Flex>
+            <FormRouter />
         </React.Fragment>
     );
 };
