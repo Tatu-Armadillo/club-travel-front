abcc98c8bb500a7de052ebbbc14d2211b46fcb6e
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-21 17:20:42 -0400

    feat: new hook

---
+++ src/shared/hooks/useComponent.ts
@@ -0,0 +1,15 @@
+import { ReactNode, useState, MouseEvent } from 'react';
+
+export const useComponent = (list: JSX.Element[]) => {
+    const [currentItem, setCurrentItem] = useState(0);
+
+    const changeComponent = (e: MouseEvent<HTMLButtonElement>) => {
+        if (e.currentTarget.name === 'newsButton') setCurrentItem(1);
+        if (e.currentTarget.name === 'destinationButton') setCurrentItem(2);
+    };
+    return {
+        currentItem,
+        currentComponent: list[currentItem],
+        changeComponent,
+    };
+};
