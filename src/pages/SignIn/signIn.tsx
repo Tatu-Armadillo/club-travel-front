f8210757ef30ddf8b7f42a4422783814b56d93ad
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-03-22 14:45:51 -0400

    feat: validate login on sign in page

--- src/pages/SignIn/signIn.tsx
+++ src/pages/SignIn/signIn.tsx
@@ -18,12 +18,14 @@ export const SignIn = () => {
         userName: '',
         password: '',
     });
+
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
         setInputValues({
             ...inputValues,
             [e.currentTarget.name]: e.currentTarget.value,
         });
     };
+
     const handleSubmit = async () => {
         const { userName, password } = inputValues;
         if (userName && password) {
@@ -46,55 +48,60 @@ export const SignIn = () => {
                     <p className='text-xl font-bold pt-1'>LOGIN</p>
                     <img src={Logo} alt='as' className='w-20 ' />
                 </span>
-
-                <form action='' className='w-full '>
-                    <div className='flex flex-col  '>
-                        <div className='flex flex-col items-center'>
-                            <label
-                                htmlFor='user'
-                                className='block text-sm font-medium leading-6  text-white'
-                            >
-                                Usuário
-                            </label>
-                            <input
-                                type='text'
-                                name='userName'
-                                id='user'
-                                value={inputValues.userName}
-                                onChange={handleInput}
-                                placeholder='Digite seu usuário...'
-                                autoComplete='given-name'
-                                className='p-2 mt-2 block w-4/6 rounded-md border-none py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                            />
+                {auth?.user ? (
+                    <p className='text-xl font-bold pt-1'>
+                        Você já está logado
+                    </p>
+                ) : (
+                    <form action='' className='w-full '>
+                        <div className='flex flex-col  '>
+                            <div className='flex flex-col items-center'>
+                                <label
+                                    htmlFor='user'
+                                    className='block text-sm font-medium leading-6  text-white'
+                                >
+                                    Usuário
+                                </label>
+                                <input
+                                    type='text'
+                                    name='userName'
+                                    id='user'
+                                    value={inputValues.userName}
+                                    onChange={handleInput}
+                                    placeholder='Digite seu usuário...'
+                                    autoComplete='given-name'
+                                    className='p-2 mt-2 block w-4/6 rounded-md border-none py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                />
+                            </div>
+                            <div className='flex flex-col items-center'>
+                                <label
+                                    htmlFor='name'
+                                    className='block text-sm font-medium leading-6 text-white'
+                                >
+                                    Senha
+                                </label>
+                                <input
+                                    type='text'
+                                    name='password'
+                                    onChange={handleInput}
+                                    value={inputValues.password}
+                                    id='password'
+                                    placeholder='Digite seu nome...'
+                                    autoComplete='given-name'
+                                    className='p-2 mt-2 block w-4/6 rounded-md border-none py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                />
+                            </div>
+                            <div className='flex flex-col items-center mt-20'>
+                                <Button
+                                    typeButton='button'
+                                    funcClick={handleSubmit}
+                                    classButton='rounded-md w-32 capitalize bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-black hover:text-orange-600focus-visible:outline focus-visible:outline-2 hover:text-white  focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                                    text={'Entrar'}
+                                />
+                            </div>
                         </div>
-                        <div className='flex flex-col items-center'>
-                            <label
-                                htmlFor='name'
-                                className='block text-sm font-medium leading-6 text-white'
-                            >
-                                Senha
-                            </label>
-                            <input
-                                type='text'
-                                name='password'
-                                onChange={handleInput}
-                                value={inputValues.password}
-                                id='password'
-                                placeholder='Digite seu nome...'
-                                autoComplete='given-name'
-                                className='p-2 mt-2 block w-4/6 rounded-md border-none py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                            />
-                        </div>
-                        <div className='flex flex-col items-center mt-20'>
-                            <Button
-                                typeButton='button'
-                                funcClick={handleSubmit}
-                                classButton='rounded-md w-32 capitalize bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-black hover:text-orange-600focus-visible:outline focus-visible:outline-2 hover:text-white  focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                                text={'Entrar'}
-                            />
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                )}
             </MainContainer>
         </div>
     );
