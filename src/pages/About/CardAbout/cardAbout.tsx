59d27032c880580cdf4f82019942792b7cfbe5c5
João Ygor <ygorvieira2002@gmail.com>
2023-03-22 19:45:56 -0400

    fix: properties styles

--- src/pages/About/CardAbout/cardAbout.tsx
+++ src/pages/About/CardAbout/cardAbout.tsx
@@ -1,14 +1,16 @@
 import { Card } from "./cardAbout.styled";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
 type Props = {
     title: string,
     paragraph: string,
-
-}
+};
 
 export const CardAbout = ({ paragraph, title }: Props) => {
+    const auth = useContext(AuthContext);
     return (
-        <Card>
+        <Card theme={auth.theme}>
             <h3>{title}</h3>
             <hr className="my-2 border-slate-500" />
             <p>{paragraph}</p>
