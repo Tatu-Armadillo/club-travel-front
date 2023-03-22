87d7cb4275d049c6f272694253fda98cc973d596
João Ygor <ygorvieira2002@gmail.com>
2023-03-22 19:44:30 -0400

    fix: new properties

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,14 @@
-import { IAuthContext } from '@/shared/Interface/IAuthContext';
 import { createContext } from 'react';
+import { IUser } from "@/shared/Interface/IUser";
+import { ITheme } from '@/shared/Interface/ITheme';
+
+export interface IAuthContext {
+    user: IUser | null;
+    signIn: (name: string, password: string) => Promise<boolean>;
+    signOut: () => void;
+    theme: ITheme | string;
+    setThemeuser: () => void;
+};
+
 
 export const AuthContext = createContext<IAuthContext>(null!);
