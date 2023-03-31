25234c6046672c3a17a74d25a9a2482d6c9e529d
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2023-03-30 21:00:15 -0400

    fix: strash

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,16 +1,16 @@
-export default {
-    title: "light",
+// export default {
+//     title: "light",
 
-    colors: {
+//     colors: {
         
-        background: "#F3F3F3",
-        backgroundCardAbout: "#032CA6",
-        backgroundCardLine: "#201B2C",
-        boxShadowCardAbout: "#000000",
-        title: "#000000",
-        paragraph: "##FFFFFF",
-        paragraphShadow: "#0000004d",
-        paragraphCardAbout: "#FFFFFF",
-        paragraphCardLine: "#FFFFFF"
-    }
-}
+//         background: "#F3F3F3",
+//         backgroundCardAbout: "#032CA6",
+//         backgroundCardLine: "#201B2C",
+//         boxShadowCardAbout: "#000000",
+//         title: "#000000",
+//         paragraph: "##FFFFFF",
+//         paragraphShadow: "#0000004d",
+//         paragraphCardAbout: "#FFFFFF",
+//         paragraphCardLine: "#FFFFFF"
+//     }
+// }
