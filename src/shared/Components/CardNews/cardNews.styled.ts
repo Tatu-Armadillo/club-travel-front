e182a2c4a423252242971e947fd1e1773c5d2da0
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2023-04-06 13:51:18 -0400

    feat: responsive home

--- src/shared/Components/CardNews/cardNews.styled.ts
+++ src/shared/Components/CardNews/cardNews.styled.ts
@@ -2,30 +2,54 @@ import styled from 'styled-components';
 
 export const BoxMain = styled.div`
     display: flex;
-    background-color: black;
-    width: auto;
+    flex-direction: column-reverse;
+    margin: 0 2.5rem;   
+
+    &&:hover { 
+        box-shadow: 5px 5px 10px #03738c;
+    }
+
+    @media (min-width: 1024px) {
+        flex-direction: row;
+    }
 `;
 
-export const BoxDetails = styled.div`
-    max-width: 60rem;
-    background-color: blue;
-    
+export const BoxContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+export const BoxTop = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 1rem;
+    padding: 0 .1rem;
     h2 { 
         font-weight: 700;
-        letter-spacing: -0.025rem;
+        letter-spacing: -0.025em;
+        font-size: 1.5rem;
+        line-height: 2rem;
+        color: rgb(17, 24, 39);
     }
+`;
+
+export const BoxMiddle = styled.div`
+    margin: 0 .5rem;
 
     p { 
         text-align: justify;
     }
 `;
 
-export const BoxImg = styled.div`
+export const BoxBottom = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 1rem 0;
 `;
 
 export const Img = styled.img`
-    max-width: 15rem;
+    height: 300px;
+    width: 25rem;
 `;
--- src/shared/Components/CardNews/cardNews.tsx
+++ src/shared/Components/CardNews/cardNews.tsx
@@ -1,30 +1,39 @@
-import { Img, BoxMain, BoxDetails, BoxImg } from './cardNews.styled';
+import { Img, BoxMain, BoxContent, BoxTop, BoxMiddle, BoxBottom } from './cardNews.styled';
 import { Button } from '@/shared/Components';
 
 interface CardNewsProps {
     url: string;
     title: string;
-    description?: string;
+    description: string;
 }
+
+function limitTxt(data: string): string {
+    return data.length > 280 ? data.substring(0, 250) + "..." : data;
+};
 
 export const CardNews = ({ url, title, description }: CardNewsProps) => {
     return (
-        <BoxMain>
-            <BoxDetails>
-                <h2 className='font-bold tracking-tight text-gray-900 sm:text-2xl'>
-                    {title}
-                </h2>
-                <hr />
-                <p>{description}</p>
-                <Button
-                    typeButton={'button'}
-                    text={'ler mais'}
-                    classButton={'rounded-md capitalize bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2  focus-visible:outline-offset-2 focus-visible:outline-indigo-600 '}
-                />
-            </BoxDetails>
-            <BoxImg>
-                <Img src={url} alt={title} className='hidden md:block' />
-            </BoxImg>
-        </BoxMain>
+        <section>
+            <BoxMain>
+                <BoxContent>
+                    <BoxTop>
+                        <h2>{title}</h2>
+                        <hr />
+                    </BoxTop>
+
+                    <BoxMiddle>
+                        <p>{limitTxt(description)}</p>
+                    </BoxMiddle>
+
+                    <BoxBottom>
+                        <Button typeButton={'button'}
+                            text={'ler mais'}
+                            classButton={'rounded-md capitalize bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2  focus-visible:outline-offset-2 focus-visible:outline-indigo-600 '} />
+                    </BoxBottom>
+                </BoxContent>
+
+                <Img src={url} alt="" className='thumbnail mx-auto' />
+            </BoxMain>
+        </section>
     );
 };
