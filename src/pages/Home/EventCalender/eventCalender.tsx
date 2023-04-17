0cfa01dfbf7b81667f91972d559b206247cde8f3
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-04-17 01:48:22 -0400

    feat: calender container component

--- src/pages/Home/EventCalender/eventCalender.tsx
+++ src/pages/Home/EventCalender/eventCalender.tsx
@@ -1,18 +1,38 @@
+import { TitleTopic } from '@/shared/Components';
 import { Flex } from '@chakra-ui/react';
+import { BsCalendar3 } from 'react-icons/bs';
 
 export const EventCalender = () => {
     return (
         <Flex
-            align='center'
-            justify='center'
+            align='start'
+            justify='start'
             width='90vw'
             wrap='wrap'
             borderRadius='1rem'
             minHeight='400px'
             margin='auto'
-            bgColor={'#99bddd'}
+            bgColor={'#03258C'}
+            padding={'15px'}
+            direction={'column'}
+            color={'#fff'}
         >
-            Comer tatu é bom
+            <Flex gap={10} align={'center'} className='mb-5'>
+                <strong className='text-4xl'>Calendário de Eventos</strong>
+                <BsCalendar3 size={'2rem'} />
+            </Flex>
+            <Flex wrap={'wrap'} gap={10}>
+                <div className='bg-white/5 hover:bg-white/30 transition-colors cursor-pointer w-48  p-3 rounded-md flex flex-col gap-1  items-start'>
+                    <div>Imagem do evento</div>
+                    <strong className='font-semibold'>Título do Evento</strong>
+                    <p className='text-sm font-semibold text-zinc-400'>
+                        Detalhes do evento
+                    </p>
+                    <p className='text-sm font-semibold text-zinc-400'>
+                        Data do Evento
+                    </p>
+                </div>
+            </Flex>
         </Flex>
     );
 };
--- src/shared/Components/TitleTopic/titleTopic.tsx
+++ src/shared/Components/TitleTopic/titleTopic.tsx
@@ -8,7 +8,7 @@ export const TitleTopic = ({ title, variableclasses }: TitleTopicProps) => {
         <span
             className={`${variableclasses} mb-5 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-orange-500 relative inline-block`}
         >
-            <span className='relative capitalize text-white text-3xl '>
+            <span className='relative capitalize text-white text-4xl '>
                 {title}
             </span>
         </span>
