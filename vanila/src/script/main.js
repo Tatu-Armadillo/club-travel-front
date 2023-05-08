5da08114361e9b1e95a8aa884c332a9c2084406c
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-08 20:50:53 -0300

    config deploy git-pages

--- vanila/src/script/main.js
+++
@@ -1,6 +0,0 @@
-import { gereneteHomePage } from './pages/home-page.js';
-
-window.addEventListener('load', () => gereneteHomePage());
-
-// document.querySelector('body').onload = () =>
-//     document.querySelector('dialog').showModal();
