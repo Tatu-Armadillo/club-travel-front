c14f5976327c1d890175f9b95b28272c8647b6cd
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-08 21:15:08 -0300

    dist deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-gh-pages",
   plugins: [react()],
   envDir: "./src/environment",
   resolve: {
