7203c5117598b278a2d3e0bd4660b690ec01057b
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-08 21:51:39 -0300

    Revert "Update vite.config.ts"
    This reverts commit ae9fce1e8ba9f7d77e55e002634c35cc97687826.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/club-travel-front",
+  base: "/vite-gh-pages",
   plugins: [react()],
   envDir: "./src/environment",
   resolve: {
