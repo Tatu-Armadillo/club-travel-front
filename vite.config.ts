3b96d59cf0c36868c34875c8a84f6a18f3dc5ad3
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-08 21:51:26 -0300

    Revert "deploy"
    This reverts commit 37d08937b70f8384ae77665e75d2b888966ddc30.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/club-travel-front/",
+  base: "/club-travel-front",
   plugins: [react()],
   envDir: "./src/environment",
   resolve: {
