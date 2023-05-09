ae9fce1e8ba9f7d77e55e002634c35cc97687826
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-08 21:21:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-gh-pages",
+  base: "/club-travel-front",
   plugins: [react()],
   envDir: "./src/environment",
   resolve: {
