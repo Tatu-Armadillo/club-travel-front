e3ab993aafa5ec9cb857c8c93e4fbbcaa825789a
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-05-17 12:17:14 -0400

    fix: rules post and add new performatic hook service

---
+++ src/shared/Interface/IReference.ts
@@ -0,0 +1,5 @@
+export interface IReference {
+    keyReference: string;
+    valor: string;
+    imageLink: string;
+}
--- src/shared/services/destinations/DestinationsService.ts
+++ src/shared/services/destinations/DestinationsService.ts
@@ -1,28 +1,34 @@
-import { instance } from "../api/api"
-import { IDestination } from "@/shared/Interface/IDestionation";
-import { IPage } from "@/shared/Interface/IPage";
+import { instance } from '../api/api';
+import { IDestination } from '@/shared/Interface/IDestionation';
+import { IPage } from '@/shared/Interface/IPage';
+import axios from 'axios';
 
 const getDestinations = async () => {
     const response = await instance.get(`/destinations`, {
         headers: {
-            "Content-Type": "application/json"
-        }
-    })
+            'Content-Type': 'application/json',
+        },
+    });
     return response.data;
 };
 
-const postDestinations = async (data: IDestination): Promise<IPage<IDestination>> => {
-    const response = await instance.post("/destinations", data);
+const postDestinations = async (
+    data: IDestination
+): Promise<IPage<IDestination>> => {
+    const response = await axios.post(
+        'http://localhost:8080/blo/destinations',
+        data
+    );
     return response.data;
 };
 
 const getDestinationsId = async (query: number) => {
-    const response = await instance.get(`/destinations/${query}`)
+    const response = await instance.get(`/destinations/${query}`);
     return response.data;
 };
 
 export const DestinationsService = {
     getDestinations,
     postDestinations,
-    getDestinationsId
-}
+    getDestinationsId,
+};
