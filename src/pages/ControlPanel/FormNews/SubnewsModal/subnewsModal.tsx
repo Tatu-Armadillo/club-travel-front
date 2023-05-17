d116f9a3fb01668e3e21841b9e6b9a5674655c42
joaoygorr <ygorvieira2002@gmail.com>
2023-05-16 20:01:55 -0400

    Update subnewsModal.tsx

--- src/pages/ControlPanel/FormNews/SubnewsModal/subnewsModal.tsx
+++ src/pages/ControlPanel/FormNews/SubnewsModal/subnewsModal.tsx
@@ -11,9 +11,8 @@ export const SubnewsModal = () => {
     };
 
     return (
-        <div className="relative z-10 hidden" aria-labelledby="modal-title" role="" aria-modal="true" id="modal">
+        <div className="relative z-10 hidden" aria-labelledby="modal-title" role="alert" aria-modal="true" id="modal">
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
             <div className="fixed inset-0 z-10 overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
@@ -33,7 +32,7 @@ export const SubnewsModal = () => {
                             </div>
                         </div>
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                            <Button typeButton={"submit"} text={"cadastrar"} classButton={"inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"} funcClick={() => handleSubnews()}/>
+                            <Button typeButton={"submit"} text={"cadastrar"} classButton={"inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"} funcClick={() => handleSubnews()} />
 
                             <Button typeButton={"button"} funcClick={() => handleHiddenModal()} text={"cancelar"} classButton={"mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto capitalize"} />
                         </div>
