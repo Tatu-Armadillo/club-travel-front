16fa69ca76b4764e82bed34f75000ed34bf0e531
joaoygorr <ygorvieira2002@gmail.com>
2023-05-18 14:49:58 -0400

    feat: new Subnews

--- src/pages/ControlPanel/FormDestinations/formDestinations.tsx
+++ src/pages/ControlPanel/FormDestinations/formDestinations.tsx
@@ -1,9 +1,7 @@
+import React from 'react';
 import { IDestination } from '@/shared/Interface/IDestionation';
-import { IReference } from '@/shared/Interface/IReference';
 import { CityService, DestinationsService } from '@/shared/services';
 import { ICity } from '@/shared/services/city/CityService';
-
-import React from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { BoxReferenceField } from './BoxReferenceField/boxReferenceField';
 import { ResponseCityItem } from './ResponseCityItem/responseCityItem';
@@ -11,27 +9,20 @@ import { ResponseCityList } from './ResponseCityList/responseCityList';
 
 export const FormDestinations = () => {
     const [listOpen, setListOpen] = React.useState(false);
-    const {
-        register,
-        handleSubmit,
-        watch,
-        setValue,
-        control,
-        formState: { errors },
-    } = useForm<IDestination>();
-    const { fields, append, remove } = useFieldArray({
+    const { register, handleSubmit, watch, setValue, control, formState: { errors }, } = useForm<IDestination>();
+    const { fields, append } = useFieldArray({
         control,
         name: 'references',
     });
 
     const [citiesList, setCitiesList] = React.useState<ICity[]>([]);
+
     const handleSearchCities = async (query: string) => {
         const response = await CityService.getCityByName(query);
         setCitiesList(response.data);
     };
 
     const createNewDestination = (data: IDestination) => {
-        console.log(data);
         const { references, nameCity } = data;
         if (!nameCity) {
             alert('Você precisa escolher uma cidade');
@@ -59,10 +50,7 @@ export const FormDestinations = () => {
 
     return (
         <div className='w-full flex mt-5 justify-center'>
-            <form
-                onSubmit={handleSubmit(createNewDestination)}
-                className='bg-gray-300 flex flex-col p-2 rounded-md w-1/2 gap-2 border-2 border-black m-2'
-            >
+            <form onSubmit={handleSubmit(createNewDestination)} className='bg-gray-300 flex flex-col p-2 rounded-md w-1/2 gap-2 border-2 border-black m-2'>
                 <div className='flex flex-col justify-center w-full items-center'>
                     <label className='text-2xs font-bold ' htmlFor='title'>
                         Cidade
@@ -74,9 +62,7 @@ export const FormDestinations = () => {
                         id='nameCity'
                         {...register('nameCity', { required: true })}
                     />
-                    {errors.nameCity && (
-                        <span>É obrigatório selecionar 1 cidade</span>
-                    )}
+                    {errors.nameCity && (<span>É obrigatório selecionar 1 cidade</span>)}
                     {citiesList.length > 1 && (
                         <ResponseCityList>
                             {citiesList.map((cities, index) => (
@@ -95,75 +81,44 @@ export const FormDestinations = () => {
                 </div>
                 <BoxReferenceField
                     callView={() => console.log(fields)}
-                    callAdd={() =>
-                        append({
-                            keyReference: '',
-                            imageLink: '',
-                            valor: '',
-                        })
-                    }
+                    callAdd={() => append({ keyReference: '', imageLink: '', valor: '', })}
                     childrens={fields.map((field, index) => {
                         return (
-                            <div
-                                key={field.id}
-                                className='flex flex-col justify-center w-full gap-2 items-center  border-2 bg-blue-400 rounded-md border-slate-100 p-2'
-                            >
+                            <div key={field.id} className='flex flex-col justify-center w-full gap-2 items-center  border-2 bg-blue-400 rounded-md border-slate-100 p-2' >
                                 <div className='flex justify-center w-full gap-2 items-center '>
-                                    <label
-                                        className='text-2xs font-bold flex justify-start'
-                                        htmlFor={`references.${index}.keyReference`}
-                                    >
+                                    <label className='text-2xs font-bold flex justify-start' htmlFor={`references.${index}.keyReference`}>
                                         Título Referência
                                     </label>
                                     <input
                                         className='w-64 md:w-1/2 p-1 rounded-md bg-slate-200 focus:bg-white border border-black'
                                         type='text'
                                         id='title'
-                                        {...register(
-                                            `references.${index}.keyReference`
-                                        )}
+                                        {...register(`references.${index}.keyReference`)}
                                     />
-                                    <label
-                                        className='text-2xs font-bold flex justify-start'
-                                        htmlFor={`references.${index}.imageLink`}
-                                    >
+                                    <label className='text-2xs font-bold flex justify-start' htmlFor={`references.${index}.imageLink`} >
                                         Link da Imagem
                                     </label>
                                     <input
                                         className='w-64 md:w-1/2 p-1 rounded-md bg-slate-200 focus:bg-white border border-black'
                                         type='text'
                                         id='title'
-                                        {...register(
-                                            `references.${index}.imageLink`
-                                        )}
+                                        {...register(`references.${index}.imageLink`)}
                                     />
                                 </div>
                                 <div className='w-full flex flex-col items-center justify-center '>
-                                    <label
-                                        className='text-2xs font-bold '
-                                        htmlFor={`references.${index}.valor`}
-                                    >
+                                    <label className='text-2xs font-bold ' htmlFor={`references.${index}.valor`}>
                                         Conteúdo
                                     </label>
-                                    <textarea
-                                        className='h-44 w-full p-1 rounded-md bg-slate-200 focus:bg-white border border-black'
-                                        id='title'
-                                        {...register(
-                                            `references.${index}.valor`
-                                        )}
-                                    />
+                                    <textarea className='h-44 w-full p-1 rounded-md bg-slate-200 focus:bg-white border border-black' id='title' {...register(`references.${index}.valor`)} />
                                 </div>
                             </div>
                         );
                     })}
                 />
                 <div className='flex justify-center'>
-                    <input
-                        type='submit'
-                        className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-3/4'
-                    />
+                    <input type='submit' className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-3/4' />
                 </div>
             </form>
-        </div>
+        </div >
     );
 };
--- src/pages/ControlPanel/FormNews/formNews.tsx
+++ src/pages/ControlPanel/FormNews/formNews.tsx
@@ -1,11 +1,11 @@
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { Button } from '@/shared/Components';
 import { INewsWithSubnews, INews } from '@/shared/Interface/INews';
 import { NewsService } from '@/shared/services';
 import { BoxContainer, BoxForms, BoxMiddle, BoxButtons, BoxInput, } from './formNews.styled';
 
 export const FormNews = () => {
-    const { register, formState: { errors }, handleSubmit, } = useForm<INewsWithSubnews>();
+    const { register, formState: { errors }, handleSubmit, control } = useForm<INewsWithSubnews>();
 
     const handlePost = (data: INewsWithSubnews) => {
         const subNewsArr: INews[] = [];
@@ -13,6 +13,15 @@ export const FormNews = () => {
         data.subNews = subNewsArr;
 
         // NewsService.postNews(data);
+    };
+
+    const { append, remove, fields } = useFieldArray({
+        control,
+        name: "subNews"
+    });
+
+    function addNewNews() {
+        append({ title: "", imageLink: "", text: "" });
     };
 
     return (
@@ -50,10 +59,50 @@ export const FormNews = () => {
                     <BoxButtons>
                         <Button typeButton={"submit"} text={"enviar"} classButton="text-white bg-green-800 p-2 rounded-md hover:bg-green-900" funcClick={handleSubmit(handlePost)} />
 
-                        <Button typeButton={"submit"} text={"enviar"} classButton="text-white bg-blue-800 p-2 rounded-md hover:bg-blue-900" />
+                        <Button typeButton={"submit"} text={"sub notícia"} classButton="text-white bg-blue-800 p-2 rounded-md hover:bg-blue-900" funcClick={() => addNewNews()} />
                     </BoxButtons>
                 </div>
             </BoxMiddle>
+            {fields.map((field, index) => {
+                return (
+                    <BoxMiddle key={field.id}>
+                        <h3>Cadastro de Sub Notícia</h3>
+                        <span></span>
+                        <BoxForms>
+                            <BoxInput>
+                                <label>
+                                    título
+                                </label>
+                                <input type="text" {...register(`subNews.${index}.title`, { required: true })} />
+                                {errors.subNews?.[index]?.title && (<span className="capitalize">campo obrigatório!</span>)}
+                            </BoxInput>
+
+                            <BoxInput>
+                                <label>
+                                    imagem
+                                </label>
+                                <input type="text" {...register(`subNews.${index}.imageLink`)} />
+                            </BoxInput>
+
+                            <BoxInput>
+                                <label>
+                                    texto
+                                </label>
+                                <textarea cols={30} rows={5} {...register(`subNews.${index}.text`, { required: true })}></textarea>
+                                {errors.subNews?.[index]?.text && (<span className="capitalize">campo obrigatório!</span>)}
+                            </BoxInput>
+                        </BoxForms>
+
+                        <div className='flex lg:justify-center sm:justify-center justify-center'>
+                            <BoxButtons>
+                                <Button typeButton={"submit"} text={"sub notícia"} classButton="text-white bg-blue-800 p-2 rounded-md hover:bg-blue-900" funcClick={() => addNewNews()} />
+
+                                <Button typeButton={"submit"} text={"remover"} classButton="text-white bg-red-800 p-2 rounded-md hover:bg-red-900" funcClick={() => remove(index)} />
+                            </BoxButtons>
+                        </div>
+                    </BoxMiddle>
+                )
+            })}
         </BoxContainer>
     );
 };
